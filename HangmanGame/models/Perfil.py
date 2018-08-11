4049c7c5db7ed82021e754bb63a544205fb62113
github-675455 <github-675455@vinicius.sl>
2018-08-11 19:54:51 -0300

    Not allow a profile to change user

--- HangmanGame/models/Perfil.py
+++ HangmanGame/models/Perfil.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Perfil(models.Model):
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
     jogos = models.IntegerField(default=0)
     perdeu = models.IntegerField(default=0)
     venceu = models.IntegerField(default=0)
