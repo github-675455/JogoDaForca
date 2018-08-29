0a6e46e3d6234306edaa99bd45733de234705568
github-675455 <github-675455@vinicius.sl>
2018-08-29 01:02:02 -0300

    fix conflicting migration files

--- Hangman/urls.py
+++ Hangman/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jogo/', include('HangmanGame.urls')),
     path('', include('HangmanGame.urls'))
 ]
